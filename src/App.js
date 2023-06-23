d2fef1a4b4e4b8ed1b0d8b2f083cb7ad9df393bf
ousmane gueye <ousmaneg186@gmail.com>
2023-06-23 22:16:59 +0000

    json server

--- src/App.js
+++ src/App.js
@@ -7,24 +7,49 @@ import Products from "./components/Products";
 import NewProduct from "./components/NewProduct";
 import { Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [currentRoute, setCurrentRoute] = useState();
+  useEffect(() => {
+    setCurrentRoute(window.location.pathname.replace("/", ""));
+  }, []);
+
   return (
     <BrowserRouter>
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
-            <Link className="nav-link" to={"/Home"}>
+            <Link
+              onClick={() => setCurrentRoute("Home")}
+              className={
+                currentRoute === "Home" ? "nav-link active" : "nav-link"
+              }
+              to={"/Home"}
+            >
               Home
             </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to={"/Products"}>
+            <Link
+              onClick={() => setCurrentRoute("Products")}
+              className={
+                currentRoute === "Products" ? "nav-link active" : "nav-link"
+              }
+              to={"/Products"}
+            >
               Products
             </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to={"/NewProduct"}>
+            <Link
+              onClick={() => setCurrentRoute("NewProduct")}
+              className={
+                currentRoute === "NewProduct" ? "nav-link active" : "nav-link"
+              }
+              to={"/NewProduct"}
+            >
               New Product
             </Link>
           </li>
